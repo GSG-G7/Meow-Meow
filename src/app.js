46af812ed3a18975dadbe72fb2b4c3932a8b306c
AlaaTaima <alaa.ktaima@gmail.com>
2019-08-06 14:31:35 +0300

    set view-engin and set views location, render home-page Relates #5

--- src/app.js
+++ src/app.js
@@ -3,6 +3,25 @@ const express = require('express');
 const exhbs = require('express-handlebars');
 
 const app = express();
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'hbs');
+
+app.engine(
+    'hbs',
+    exhbs({
+        extname:'hbs',
+        layoutsDir:path.join(__dirname,'views','layouts'),
+        partialsDir: path.join(__dirname, 'views', 'partials'),
+        defaultLayout: 'main'
+    })
+)
+
+app.get('/',(req,res)=> {
+    res.render('home',{
+        appName: 'Cat Photos App'
+    })
+})
+
 
 app.set('port', process.env.PORT || 5000)
 
