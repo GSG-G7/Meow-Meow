b5c112b8d1f5a9f276c4bde394f247a22c44ad7f
AlaaTaima <alaa.ktaima@gmail.com>
2019-08-07 15:40:14 +0300

    refactor-code to get required data and render it Relates #14

--- src/controlers/request.js
+++ src/controlers/request.js
@@ -3,12 +3,12 @@ const fetch = require('node-fetch');
 
 exports.post = (req,response)=> {
     const value =req.body.input;
-    const url = `https://api.thecatapi.com/v1/images/search?limit=3&key=${process.env.MEOW_API_KEY}q=${value}`
+    const url = `https://api.thecatapi.com/v1/images/search??breed_ids=${value}&limit=4&key=${process.env.MEOW_API_KEY}`
     fetch(url)
     .then(res=> res.json())
     .then(res=> response.render('home',{
-        image : res[0].url,
-        name : res[0].breeds[0].name
+        cats : res,
+        appName : 'Cat Photo App'
     }))
 } 
 
--- src/test/test.js
+++ src/test/test.js
@@ -16,3 +16,25 @@ test("testing for status code 404",t=>{
     })
 })
 
+test("testing for rout  / ",t=>{
+    supertest(app)
+    .get('/')
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err,res)=> {
+        t.error(err)
+        t.end()
+    })
+})
+
+test("testing for rout  /search ",t=>{
+    supertest(app)
+    .post('/search')
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err,res)=> {
+        t.error(err)
+        t.end()
+    })
+})
+
